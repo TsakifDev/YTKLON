28391e37c13658973ecbd527dff9c1f5ee3f0272
my name Mr.Tsakif <65777738+ayamandul@users.noreply.github.com>
2021-04-21 14:01:53 +0700

    Add files via upload

---
+++ yt klon/Youtube Clone/Helper/Constants.swift
@@ -0,0 +1,18 @@
+//
+//  Constants.swift
+//  Youtube Clone
+//
+//  Created by ADMIN on 17/03/21.
+//
+
+import Foundation
+
+struct Constants {
+  static var API_KEY = "AIzaSyAVpl2CugmYOTgybzFFeh2zIPnJZbZJ6fo"
+  static var PLAYLIST_ID = "UUb5rk08d0Db4Ss0DqnMIAZQ&index=2"
+  static var API_URL = "https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=\(Constants.PLAYLIST_ID)&key=\(Constants.API_KEY)"
+  static var VIDEOCEL_ID = "VideoCell"
+static var YT_EMBED_URL = "https://www.youtube.com/embed/"
+
+}
+//ini untuk menyimpan link
